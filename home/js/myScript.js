fe29530e3075f4bb2994c31e9ab2d950526452f6
apcagcatap <agcatap@student.apc.edu.ph>
2024-12-17 09:11:29 +0800

    om

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -5,5 +5,5 @@ function myFunction() {
 function myFunction2() {
     const element = document.getElementsByTagName("p");
 
-    document.getElementById("demo1").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
 }
