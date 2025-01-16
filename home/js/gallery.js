010f9b0fe76edef1f8de817dbd3f021a99d9c061
apcagcatap <agcatap@student.apc.edu.ph>
2025-01-17 06:05:10 +0800

    web

---
+++ home/js/gallery.js
@@ -0,0 +1,50 @@
+Vue.component('photo-carousel', {
+    data: function() {
+      return {
+        images: [
+          'img_5terre.jpg',
+          'img_forest.jpg',
+          'img_forest.jpg',
+          'img_5terre.jpg',
+        ],
+        currentIndex: 0
+      };
+    },
+    computed: {
+      transformStyle() {
+        return `translateX(-${this.currentIndex * 100}%)`;
+      }
+    },
+    methods: {
+      nextImage() {
+        if (this.currentIndex < this.images.length - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0; // Loop back to the first image
+        }
+      },
+      prevImage() {
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.images.length - 1; // Loop back to the last image
+        }
+      }
+    },
+    template: `
+      <div class="carousel">
+        <div class="carousel-images" :style="{ transform: transformStyle }">
+          <img v-for="(image, index) in images" :key="index" :src="image" :alt="'Image ' + (index + 1)">
+        </div>
+        <div class="carousel-buttons">
+          <button @click="prevImage">❮</button>
+          <button @click="nextImage">❯</button>
+        </div>
+      </div>
+    `
+  });
+
+  // Vue Instance
+  new Vue({
+    el: '#app',
+  });
